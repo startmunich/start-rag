a697bceabe78076ec79e65965accf438fe75cf8e
Khadim Fall <khadimfall0602@gmail.com>
2024-03-13 20:33:01 +0100

    :)

--- apps/dashboard/src/app/page.tsx
+++ apps/dashboard/src/app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
     <main className="h-[100vh]">
-      
+      <div className="flex items-center justify-center h-full">
+        <h1 className="text-3xl font-bold text-center text-gray-900">
+          Notion Crawler 👋🏽
+        </h1>
+      </div>
     </main>
   );
 }
